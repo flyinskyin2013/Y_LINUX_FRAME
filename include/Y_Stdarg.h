0c30713fe2d16d9bed56747d1b5fa920fa0f3d77
Sky <1006332647@qq.com>
2016-02-28 17:52:55 +0800

    add sharelib and staticlib,meanwhile,update makefile surport those command

--- include/Y_Stdarg.h
+++ include/Y_Stdarg.h
@@ -16,9 +16,7 @@
 
 #ifdef Y_TEST_VA
 
-
 typedef char * y_va_list;
-
 
 /*
 *	Storage alignment properties
@@ -29,18 +27,13 @@ typedef char * y_va_list;
 
 #define Y_ADNBND (sizeof (int) - 1) 
 
-
 /*
 *	Variable argument list macro definitions
 */
 
-
 #define y_bnd(X, bnd) (((sizeof (X)) + (bnd)) & (~(bnd))) //memory align
-
 #define y_va_start(ap,A) (void) ( (ap) = ( ((y_va_list)&(A)) + (y_bnd(A,Y_AUPBND)) ) )//Making ap point at &A + 4
-
 #define y_va_arg(ap,T) (*(T *)( ( (ap) += (y_bnd(T,Y_AUPBND)) ) - (y_bnd(T,Y_AUPBND)) ))//get value of ap,and ap + 4
-
 #define y_va_end(ap)  ( (ap) = (y_va_list)0 ) 
 
 
@@ -51,9 +44,7 @@ typedef __builtin_va_list __gnuc_va_list;
 typedef __gnuc_va_list y_va_list;
 
 #define y_va_start(ap,A) __builtin_va_start(ap,A)
-
 #define y_va_arg(ap,T) __builtin_va_arg(ap,T)
-
 #define y_va_end(ap)  __builtin_va_end(ap)
 
     
