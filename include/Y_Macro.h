e6ab7ea1689241e528b69aae41b870882aa2aa3f
Sky <1006332647@qq.com>
2016-03-03 14:38:11 +0800

    redefine two macro,but I alse use first defination

--- include/Y_Macro.h
+++ include/Y_Macro.h
@@ -1,9 +1,9 @@
 /*
 *	FileName:Y_Macro.h
-*	Version: 1.2	
+*	Version: 1.3	
 *	Description: The file defines my favorite macroes.
 *	Created On:2016-2-16
-*	Modified date:2016-2-22
+*	Modified date:2016-3-3
 *	Author:Sky
 *
 */
@@ -18,6 +18,9 @@
 #define PROGRAM_END 0x01
 
 //If we don't use memset(),we will get a unkown error.Here,I miss a '\n',when I use fork().Because the achieve of strcat() is crazy. 
+
+// This is my first to implement those macroes,It looks difficult.but,It make code clear
+
 #define ERROR_MSG(where,what) do{ \
 	char * addr; \
 	addr = malloc(sizeof(char)*128); \
@@ -34,6 +37,17 @@
 	Y_PRINTF("%s",strcat(strcat(addr,(what) ),"\n")); \
 	free(addr); \
 	} while(0)
+
+
+
+// This is my second to implement those macroes,It looks difficult.It make code confuse,so I use my first type
+/*
+#define ERROR_MSG(where,what) \
+	Y_PRINTF(#where" is error, The error is "#what"\n")
+#define SHOW_MSG(what) \
+	Y_PRINTF(#what"\n")
+*/
+
 
 
 #endif//_Y_MACRO_H
